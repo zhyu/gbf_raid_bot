506b8b1b5ff30404d0572987758574085a50adee
zhyu <angellwings@gmail.com>
2017-04-04 14:29:38 +0900

    feat: store target bosses to redis

--- lib/gbf_raid_bot/repo.ex
+++ lib/gbf_raid_bot/repo.ex
@@ -7,4 +7,20 @@ defmodule GbfRaidBot.Repo do
     {:ok, boss_names} = Redix.command(:redix, ~w(SMEMBERS boss_name))
     Enum.sort(boss_names)
   end
+
+  def store_target_boss(boss_name) when is_binary(boss_name) do
+    Redix.command(:redix, ~w(SADD target_boss) ++ ["#{boss_name}"])
+  end
+
+  def list_up_target_bosses do
+    {:ok, boss_names} = Redix.command(:redix, ~w(SMEMBERS target_boss))
+    Enum.sort(boss_names)
+  end
+
+  def target_boss?(boss_name) do
+    case Redix.command(:redix, ~w(SISMEMBER target_boss) ++ ["#{boss_name}"]) do
+      {:ok, 1} -> true
+      _ -> false
+    end
+  end
 end
--- test/gbf_raid_bot/repo_test.exs
+++ test/gbf_raid_bot/repo_test.exs
@@ -15,4 +15,17 @@ defmodule GbfRaidBot.RepoTest do
     Repo.store_boss_name("Lv50 ティアマト・マグナ")
     assert Repo.list_up_boss_names() == ["Lv50 ティアマト・マグナ", "Lv75 セレスト・マグナ"]
   end
+
+  test "target bosses" do
+    Repo.store_target_boss("Lv75 セレスト・マグナ")
+
+    assert Repo.target_boss?("Lv75 セレスト・マグナ")
+    refute Repo.target_boss?("Lv50 ティアマト・マグナ")
+    assert Repo.list_up_target_bosses() == ["Lv75 セレスト・マグナ"]
+
+    Repo.store_target_boss("Lv50 ティアマト・マグナ")
+
+    assert Repo.target_boss?("Lv50 ティアマト・マグナ")
+    assert Repo.list_up_target_bosses() == ["Lv50 ティアマト・マグナ", "Lv75 セレスト・マグナ"]
+  end
 end
